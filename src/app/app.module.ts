4b15cff1026801702b71a74fee6686bc4ca78904
thanhbinh <thanhbinhgtv@gmail.com>
2021-03-10 11:16:10 +0700

    structure component admin, auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminComponent } from './components/admin/admin.component';
+import { HeaderComponent } from './components/admin/shared/header/header.component';
+import { FooterComponent } from './components/admin/shared/footer/footer.component';
+import { SlidebarComponent } from './components/admin/shared/slidebar/slidebar.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SingupComponent } from './components/auth/singup/singup.component';
+import { ViewStaffComponent } from './components/admin/staff-manager/view-staff/view-staff.component';
+import { CreateStaffComponent } from './components/admin/staff-manager/create-staff/create-staff.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    HeaderComponent,
+    FooterComponent,
+    SlidebarComponent,
+    LoginComponent,
+    SingupComponent,
+    ViewStaffComponent,
+    CreateStaffComponent,
   ],
   imports: [
     BrowserModule,
