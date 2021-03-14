f89f26e21ed701372a5a2061f66ce35d325f0e21
thanhbinh <binhpt.bhsoft@gmail.com>
2021-03-14 16:47:07 +0700

    Test structure staff

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { HomeComponent } from './home/home.component';
+import { ViewStaffComponent } from './staff-manager/view-staff/view-staff.component';
 
 const routes: Routes = [{
   path: '', component: AdminComponent,
   children: [
     {
       path: 'home-admin', component: HomeComponent,
+    },
+    {
+      path: 'view-staff', component: ViewStaffComponent,
     },
     
     { path: '**', redirectTo: 'home-admin' },
