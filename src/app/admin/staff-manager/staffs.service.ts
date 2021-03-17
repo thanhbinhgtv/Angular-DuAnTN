f681d801fb8b9ff09bd34529ef69c5a848f47573
thanhbinh <binhpt.bhsoft@gmail.com>
2021-03-18 00:54:43 +0700

    conplete function delete Staff

--- src/app/admin/staff-manager/staffs.service.ts
+++ src/app/admin/staff-manager/staffs.service.ts
@@ -13,4 +13,8 @@ export class StaffsService {
   getAllStaffs(): Observable<Array<StaffsModel>> {
     return this.http.get<Array<StaffsModel>>('http://localhost:8080/super-admin/staffs');
   }
+  
+  deleteStaff(id: number): Observable<any> {
+    return this.http.delete('http://localhost:8080/super-admin/staffs/' + id);
+  }
 }
--- src/app/admin/staff-manager/view-staff/view-staff.component.ts
+++ src/app/admin/staff-manager/view-staff/view-staff.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { StaffsService } from '../staffs.service';
 import { StaffsModel } from './staffs-model';
 
@@ -9,14 +10,31 @@ import { StaffsModel } from './staffs-model';
 })
 export class ViewStaffComponent implements OnInit {
   staffs: Array<StaffsModel> = [];
+  deleteSuccess : boolean;
 
-  constructor(private staffService: StaffsService) { 
+  constructor(private staffService: StaffsService, private toastr: ToastrService) { 
+  }
+
+  ngOnInit(): void {
+    this.getAllStaff();
+  }
+
+  getAllStaff(){
     this.staffService.getAllStaffs().subscribe(staff =>{
       this.staffs = staff;
     });
   }
 
-  ngOnInit(): void {
+  deleteStaff(id: number){
+    this.staffService.deleteStaff(id).subscribe(() =>{
+      this.toastr.success('Xóa thành công')
+      this.deleteSuccess = true;
+      if(this.deleteSuccess){
+        this.getAllStaff();
+      }
+    }, () => {
+      this.toastr.error('Xóa thất bại! Vui lòng kiểm tra lại');
+  });
   }
 
 }
