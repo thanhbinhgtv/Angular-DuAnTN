9c3fd1c21d1041a51b359e33c29ca51b34a2e1ac
thanhbinh <binhpt.bhsoft@gmail.com>
2021-03-18 09:43:42 +0700

    handle true - false and icon house

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -11,9 +11,9 @@ import { AuthService } from '../auth/service/auth.service';
               <!-- Sidebar - Brand -->
               <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                   <div class="sidebar-brand-icon rotate-n-15">
-                      <i class="fas fa-laugh-wink"></i>
+                     <i class="fas fa-home"></i>
                   </div>
-                  <div class="sidebar-brand-text mx-3">Admin<sup>123</sup></div>
+                  <div class="sidebar-brand-text mx-3">Thanh Xuân</div>
               </a>
               <!-- Divider -->
               <hr class="sidebar-divider my-0">
@@ -21,7 +21,7 @@ import { AuthService } from '../auth/service/auth.service';
               <li class="nav-item active">
                   <a class="nav-link" href="index.html">
                       <i class="fas fa-fw fa-tachometer-alt"></i>
-                      <span>Dashboard</span></a>
+                      <span>Bảng Cá Nhân</span></a>
               </li>
               <!-- Divider -->
               <hr class="sidebar-divider">
@@ -73,11 +73,11 @@ import { AuthService } from '../auth/service/auth.service';
                   <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
                       aria-expanded="true" aria-controls="collapsePages">
                       <i class="fas fa-fw fa-folder"></i>
-                      <span>Danh Sách</span>
+                      <span>Quản Lý</span>
                   </a>
                   <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
                       <div class="bg-white py-2 collapse-inner rounded">
-                          <h6 class="collapse-header">Quản Lý:</h6>
+                          <h6 class="collapse-header">Danh Sách:</h6>
                           <a class="collapse-item" routerLink="staff">Nhân Viên</a>
                           <a class="collapse-item" href="register.html">Bài Đăng</a>
                           <a class="collapse-item" href="forgot-password.html">Khách Hàng</a>
@@ -142,14 +142,10 @@ import { AuthService } from '../auth/service/auth.service';
               <!-- Divider -->
               <hr class="sidebar-divider d-none d-md-block">
               <!-- Sidebar Toggler (Sidebar) -->
-              <div class="text-center d-none d-md-inline">
-                  <button class="rounded-circle border-0" id="sidebarToggle"></button>
-              </div>
               <!-- Sidebar Message -->
-              <div class="sidebar-card">
-                  <img class="sidebar-card-illustration mb-2" src="assets/img/undraw_rocket.svg" alt="">
-                  <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-                  <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
+              <div class="sidebar-card" [ngStyle]="{'background-color':'#313D60'}">
+                  <img class="sidebar-card-illustration mb-2" src="assets/img/icon-house.png" alt="" [ngStyle]="{'height': '80px'}">
+                <a class="btn btn-secondary btn-sm" href="#" [ngStyle]="{'background-color':'#37446B'}">Trang Chủ</a>
               </div>
           </ul>
           <!-- End of Sidebar -->
